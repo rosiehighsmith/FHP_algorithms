631dccc9443ceaabdf26ef9b1d7e9f90fa17877e
Rosie <ariel.highsmith@gmail.com>
2016-11-30 11:58:54 -0800

    Refactor original luhn.rb

--- Luhn/lib/luhn.rb
+++ Luhn/lib/luhn.rb
@@ -1,11 +1,9 @@
 module Luhn
   def self.is_valid?(number)
     # breaks number into individual digits
-    digits = number.to_s.chars.map(&:to_i)
-    blam = digits.reverse
-    sum = 0
-    i = 0
-    blam.each do |n|
+    digits = number.to_s.chars.map(&:to_i).reverse
+    sum, i = 0, 0
+    digits.each do |n|
       n*=2 if i.odd?
       n = (n-9) if n > 9
       sum += n
