795ba1039276d79b7a88f386ceb74334846062f4
Rosie <ariel.highsmith@gmail.com>
2016-11-30 13:48:21 -0800

    Luhn with different loop methods

---
+++ Luhn/lib/luhn-eachwindex.rb
@@ -0,0 +1,17 @@
+# luhn solution with each_with_index loop
+module Luhn
+  def self.is_valid?(number)
+    # breaks number into individual digits
+    digits = number.to_s.chars.map(&:to_i).reverse
+    sum = 0
+    digits.each_with_index do |n, i|
+      n*=2 if i.odd?
+      n = (n-9) if n > 9
+      sum += n
+    end
+    sum % 10 == 0
+  end
+end
+
+puts Luhn.is_valid?(4194560385008504)
+puts Luhn.is_valid?(4194560385008505)
--- Luhn/lib/luhn-forloop.rb
+++ Luhn/lib/luhn-forloop.rb
@@ -1,9 +1,10 @@
-# luhn solution with each loop
+# luhn solution with for loop
 module Luhn
   def self.is_valid?(number)
+    # breaks number into individual digits
     digits = number.to_s.chars.map(&:to_i).reverse
     sum, i = 0, 0
-    digits.each do |n|
+    for n in digits do 
       n*=2 if i.odd?
       n = (n-9) if n > 9
       sum += n
@@ -12,4 +13,8 @@ module Luhn
     sum % 10 == 0
   end
 end
+
+puts Luhn.is_valid?(4194560385008504)
+puts Luhn.is_valid?(4194560385008505)
+
 
