d7b752a992df1b1b997f6c29870d384008e43355
Rosie <ariel.highsmith@gmail.com>
2016-12-05 20:08:55 -0800

    Remove  link from fib.rb

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -21,4 +21,4 @@ n = 35
 Benchmark.bm do |x|
   x.report("recursive_fib") { recursive_fib(n) }
   x.report("iterative_fib")  { iterative_fib(n)  }
-endhttps://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&ved=0ahUKEwjnnNWQkdbQAhXGz1QKHTEyB-0QFgggMAE&url=http%3A%2F%2Fstackoverflow.com%2Fquestions%2F10197254%2Fcase-statement-with-multiple-values-in-each-when-block&usg=AFQjCNEEclUDu98OEKdZbnHr_WfTUP9q4A&sig2=gtbpi0Po5lYWcasZrTsWQA&bvm=bv.139782543,d.cGw
+end
