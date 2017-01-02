aa5d16d107a5685d7544cc7c8be86184d7e7f51d
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-01-02 14:23:16 -0800

    add levenshteinDistance solution

---
+++ levenshteinDistance.py
@@ -0,0 +1,14 @@
+def levenshteinDistance(str1, str2):
+    if len(str1) > len(str2):
+        str1, str2 = str2, str1
+
+    distances = range(len(str1) + 1)
+    for i2, c2 in enumerate(str2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(str1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]`
