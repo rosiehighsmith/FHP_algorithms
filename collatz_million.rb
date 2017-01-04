279cef59b1b62f03019c8d1308748c9a368d173f
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-01-04 09:11:01 -0800

    complete collatz to find longest up to million

--- collatz_million.rb
+++ collatz_million.rb
@@ -1,25 +1,17 @@
-# Find the input n that produces the longest chain 
-# of collatz values from n = 1 to 1,000,000. 
-# n → n/2 (n is even)
-# n → 3n + 1 (n is odd)
-
-n = 4
-array = []
-array << n
-
-for n in 1..10 do
-  collatz(n)
-end
-
-def collatz(n)
-  while n > 1
-    if n.even?
-      n = n/2
-    else
-      n = 3*n +1
+def longest_collatz_sequence
+  longest_sequence = []
+  (1..1000000).each do |array|
+    current_sequence = [array]
+    until current_sequence.last == 1
+      if current_sequence.last.even?
+        current_sequence << current_sequence.last/2
+      else
+        current_sequence << (3 * current_sequence.last + 1)
+      end
     end
-    array << n
+    longest_sequence = current_sequence if current_sequence.length > longest_sequence.length
   end
-  puts array.length
+  longest_sequence.length
 end
 
+print longest_collatz_sequence
