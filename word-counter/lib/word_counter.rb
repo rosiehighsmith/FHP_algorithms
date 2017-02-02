135bdb6520e29f9b5f5bf7767ef4737f31393284
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-02-02 10:04:28 -0800

    split into hash table with counts without punctuation removed

--- word-counter/lib/word_counter.rb
+++ word-counter/lib/word_counter.rb
@@ -1,8 +1,10 @@
 module WordCounter
 
 	def self.count(string)
-		# Implement Me!
-		{}
+    return nil if string.nil? or string == ""
+    string.downcase.gsub!(/[^a-z0-9\s"\'"\-]/i, '')
+    result = string if string.split == string
+    result = string.split.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
 	end
 
 end
